250c4705bf34208be42528aba8255c200a532073
joaopedro <joaopkrodrigues@gmail.com>
2025-08-12 21:49:53 -0300

    Feat: Subindo a criação do comando sync:posts, a criação e ajustes da Model e Migrations. Já feito também o guzzle consumindo a API externa e salvando no meu banco de Dados

---
+++ teste-centrionweb/app/Models/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+      protected $fillable = [
+        'userId',
+        'title',
+        'body',
+    ];
+}
